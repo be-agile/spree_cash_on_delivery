7157817db9901d595e34861bc2d59e6d3c52d579
Zaher Hazeem <zhazeem@gmail.com>
2021-05-07 14:50:13 +0300

    override payment model

--- app/models/spree_cash_on_delivery/payment_decorator.rb
+++ app/models/spree_cash_on_delivery/payment_decorator.rb
@@ -1,20 +1,19 @@
-Spree::Payment.class_eval do
-
-
+module SpreeCashOnDelivery::Spree::PaymentDecorator
   def build_source
     return unless new_record?
 
     if source_attributes.present? && source.blank? && payment_method.try(:payment_source_class)
       self.source = payment_method.payment_source_class.new(source_attributes)
-      self.source.payment_method_id = payment_method.id
-      self.source.user_id = self.order.user_id if self.order
+      source.payment_method_id = payment_method.id
+      source.user_id = order.user_id if order
     end
-
+    
     # for Cash on Delivery
     if payment_method and payment_method.respond_to?(:post_create)
       payment_method.post_create(self)
     end
 
   end
-
 end
+
+Spree::Payment.prepend SpreeCashOnDelivery::Spree::PaymentDecorator unless Spree::Payment.included_modules.include?(SpreeCashOnDelivery::Spree::PaymentDecorator)
